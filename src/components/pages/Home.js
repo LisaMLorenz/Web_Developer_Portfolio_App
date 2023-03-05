61a8b6a6896ab9e956bee8ebcfa6fb7898192385
LisaMLorenz <hej@frau-lorenz.de>
2023-03-05 00:13:06 +0000

    all project info added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h1>Welcome</h1>
-      <p>This is a welcome message.</p>
+      <h1>Welcome to my Dev Portfolio</h1>
+      <p>Code & Design. I can help you jump-start the success of your passion projects and business. With over 10 years of experience in freelance graphic design with a focus on the creative and social justice sector. Get in touch to discuss your ideas and needs!</p>
     </div>
   )
 };
